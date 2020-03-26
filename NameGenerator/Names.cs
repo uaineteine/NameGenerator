dd9881a92f4774a5cbfeebcf89a475c0b3f05a83
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2020-03-26 22:58:11 +1100

    name generator library licence and readme added

---
+++ NameGenerator/Names.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Globalization;
+using csvf;
+
+namespace Namelists
+{
+    static class Names
+    {
+        static Random rndm = new Random();
+        static string[] MaleFirstNames;
+        static string[] FemaleFirstNames;
+        static string[] LastNames;
+        static string[] TownNB;
+        static string[] TownNE;
+        static string[] ProvinceNE;
+        static object[,] TitleData;
+
+        public static void Load()
+        {
+            string filename = "Namelists/LastNameList.csv";
+            LastNames = CSV.ReadAllSC(filename).Cast<string>().ToArray();
+            filename = "Namelists/MaleFirstNames.csv";
+            MaleFirstNames = CSV.ReadAllSC(filename).Cast<string>().ToArray();
+            filename = "Namelists/FemaleFirstNames.csv";
+            FemaleFirstNames = CSV.ReadAllSC(filename).Cast<string>().ToArray();
+            filename = "Namelists/TownNameBeggining.csv";
+            TownNB = CSV.ReadAllSC(filename).Cast<string>().ToArray();
+            filename = "Namelists/TownNameEnding.csv";
+            TownNE = CSV.ReadAllSC(filename).Cast<string>().ToArray();
+            filename = "Namelists/ProvinceNameEnding.csv";
+            ProvinceNE = CSV.ReadAllSC(filename).Cast<string>().ToArray();
+            filename = "Namelists/Titles.csv";
+            TitleData = CSV.ReadAllMC(filename);
+        }
+        public static bool TitlePassesDown(int type)
+        {
+            if (Convert.ToInt32(TitleData[type, 4]) != -1)
+                return true;
+            return false; //else
+        }
+        public static int GetChildType(int currentType)
+        {
+            return Convert.ToInt32(TitleData[currentType, 4]);
+        }
+        public static string GetFirstName(int Gender)//0 m 1 f
+        {
+            if (Gender == 1)
+                return FemaleFirstNames[rndm.Next() % FemaleFirstNames.Count()];
+            //else
+            return MaleFirstNames[rndm.Next() % MaleFirstNames.Count()];
+        }
+        public static float GetTitlePower(int type)
+        {
+            return Convert.ToSingle(TitleData[type, 3]);
+        }
+        public static string GetTitleName(int type, int MorF)
+        {
+            int morf = 1;
+            if (MorF == 1)
+                morf = 2;
+            return (string)TitleData[type, morf];
+        }
+        public static string GetLastName()
+        {
+            return LastNames[rndm.Next() % LastNames.Count()]; 
+        }
+        public static string GetTownName(string basename, int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    return "The Town of " + basename;
+                    break;
+                case 1:
+                    return "The Village of " + basename;
+                    break;
+                case 2:
+                    return "The City of " + basename;
+                    break;
+                case 3:
+                    return "Barony of " + basename;
+                    break;
+                case 4:
+                    return "Fort " + basename;
+            }
+            return "Townname";
+        }
+        public static string GetProvinceName(string basename, int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    return "State of " + basename;
+                case 2:
+                    return "Duchy of " + basename;
+            }
+            //default
+            return basename;
+        }
+        public static string GetNationName(string basename, int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    return "The Kingdom of " + basename;
+                    break;
+                case 1:
+                    return "The adjective " + basename;
+                    break;
+                case 3:
+                    return "The " + basename + " Republic";
+                    break;
+                case 4:
+                    return "The Principality of " + basename;
+                    break;
+                case 5:
+                    return "The " + basename + " Confederacy";
+                    break;
+                case 6:
+                    return "The State of " + basename;
+                    break;
+                case 7:
+                    return "The Sultanate of " + basename;
+                    break;
+                case 8:
+                    return "The Commonwealth of " + basename;
+                    break;
+            }
+            return basename;
+        }
+        public static string GenerateProvinceName()
+        {
+            string fullname = "";
+            string forname = "";
+            int one = rndm.Next() % 10;
+            if (one == 1)
+            {
+                //use first names
+                int gen = rndm.Next() % 2;
+                forname = GetFirstName(gen);
+            }
+            else
+            {
+                //use extra town names
+                int nameNo = rndm.Next() % TownNB.Length;
+                forname = TownNB[nameNo];
+            }
+            int LastNameNo = rndm.Next() % ProvinceNE.Length;
+            fullname = forname + ProvinceNE[LastNameNo];
+            return fullname;
+        }
+        public static string GenerateTownName()
+        {
+            string fullname = "";
+            string forname = "";
+            int one = rndm.Next() % 10;
+            if (one == 1)
+            {
+                //use first names
+                int gen = rndm.Next() % 2;
+                forname = GetFirstName(gen);
+            }
+            else
+            {
+                //use extra town names
+                int nameNo = rndm.Next() % TownNB.Length;
+                forname = TownNB[nameNo];
+            }
+            int LastNameNo = rndm.Next() % TownNE.Length;
+            fullname = forname + TownNE[LastNameNo];
+            return fullname;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+# NameGenerator
+
+A C# name generator library that returns either town names, place names or prefix titles based on CSV documents supplied.
+
+## Getting Started
+
+Compling yourself: Use Visual Studio and the project file to create either a new project to be modified or add to a solution for use as a DLL.
+
+### Requires:
+
+This project is target for .NET core 2.1. It should be compliant with earlier versions of course, however when compiling with VS, one will have to change the project settings.
+
+### Built With
+
+* [CSVHandler](https://bitbucket.org/uaineteinestudio/csvhandler) by [UaineTeine](https://bitbucket.org/uaineteinestudio/) - The INI file parser library which is required to compile the software.
+
+### Installing
+
+##### Building with Windows:
+
+Use Visual Studio to build the project file into an executable, deploy to the working directory of any future project you are making.
+
+##### Building with Linux:
+
+Open terminal/cmd and use .NET core to build with:
+
+```
+dotnet publish -c release -r ubuntu.16.04-x64 --self-contained
+```
+
+For either platform the built app into your working directory. Git creditional storing is required in that project folder. Set with:
+```
+git config credential.helper store
+```
+
+## Authors
+
+* **Daniel Stamer-Squair** - *UaineTeine*
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
